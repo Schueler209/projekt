8dad4044545cae792c68228f952b9c382a7d5978
Leo Voss <leo-voss02@gmx.de>
2019-09-30 21:51:49 +0200

    Leos Teil

---
+++ Connector/internal/Class1.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Class1
+    Public Sub sendRegister(benutzername As String, passwort As String)
+        'Als function die wenn positive rückmeldung von server kriegt als rückgabewert bestätigung gibt
+        'laura dann if besagte function = bestätigt then registrierung erfolgreich
+
+        ClientConnector.send(zusammengeschrieben(benutzername, passwort))
+
+
+    End Sub
+
+
+
+
+
+
+
+    Public Function zusammenschreiben(ByVal benutzername As String, ByVal passwort As String) As String
+        'Benutzername und Passwort als ein String zum versenden
+        'x vorweg damit man weiss wieviele zeichen benutzername hat und wo passwort beginnt
+        Dim x As Integer
+        benutzername.Length = x
+        zusammenschreiben = x & benutzername & passwort
+    End Function
+
+
+    Public Sub trennen(ByVal zusammengeschrieben)
+        '2 Rückgabewerte: Benutzername, Passwort, wie umsetzen?
+
+
+
+    End Sub
+End Class
+
