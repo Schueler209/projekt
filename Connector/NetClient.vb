2ef1917a00fadb6288de3af71759ea17b9a10946
Schueler209 <schueler109@jbs-pinneberg.de>
2019-10-16 15:15:25 +0200

    + NetClient

--- Connector/NetClient.vb
+++ Connector/NetClient.vb
@@ -1,3 +1,67 @@
-﻿Public Class NetClient
-    Public ReadOnly OnRecieve As EventNotifier(Of ConnectionData) = New EventNotifier(Of ConnectionData)
+﻿Imports System.Net.Sockets
+
+Public Class NetClient
+
+    Private connector As ClientConnector
+
+    Sub New()
+    End Sub
+
+    ' Erstellt neue Instanz und verbindet sich
+    Public Sub connect()
+        connector = New ClientConnector()
+        ' Einkommende Nachricht handeln
+        connector.OnRecieve.addHandler(AddressOf onRequest)
+        connector.connect()
+    End Sub
+
+    ' Event für Registrierung Neue Methode zuweisen!
+    Public OnRegisterConfirm As Action(Of Boolean)
+    ' Event für login Neue Methode zuweisen!
+    Public OnLoginConfirm As Action(Of Boolean)
+
+    ' Falls neue Nachricht kommt:
+    Private Sub onRequest(req As ConnectionData)
+        ' Welcher Typ ist die Nachricht?
+        Select Case req.Type
+            Case "registerconfirm"
+                If OnRegisterConfirm IsNot Nothing Then
+                    ' Argumente bekommen
+                    Dim res As String = req.Data.Item("success")
+
+                    ' Methode aufrufen + Callback 
+                    OnRegisterConfirm(res)
+                End If
+            Case "loginconfirm"
+                If OnLoginConfirm IsNot Nothing Then
+                    ' Argumente bekommen
+                    Dim res As String = req.Data.Item("success")
+                    ' Methode aufrufen + Callback 
+                    OnLoginConfirm(res)
+                End If
+        End Select
+
+    End Sub
+
+    ' Registrieren
+    Sub Register(name As String, username As String, password As String, callback As Action(Of Boolean))
+        Dim data As New Dictionary(Of String, Object)
+        data.Add("name", name)
+        data.Add("username", username)
+        data.Add("password", password)
+        Dim req As New ConnectionData("register", data)
+        connector.send(req)
+        ' Callback setzen
+        OnRegisterConfirm = callback
+    End Sub
+    ' Einloggen
+    Sub Login(username As String, password As String, callback As Action(Of Boolean))
+        Dim data As New Dictionary(Of String, Object)
+        data.Add("username", username)
+        data.Add("password", password)
+        Dim req As New ConnectionData("loginconfirm", data)
+        connector.send(req)
+        ' Callback setzen
+        OnLoginConfirm = callback
+    End Sub
 End Class
