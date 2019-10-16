6e50e40bffa74b2f5e3bb14d4b2dc9195d93c774
Till Gruschka <till.gruschka@icloud.com>
2019-10-16 12:35:00 +0200

    Register Methode

--- Server/Module1.vb
+++ Server/Module1.vb
@@ -1,7 +1,28 @@
 ﻿Module Module1
+    
+    Public Sub Main()
 
-    Sub Main()
+    End Sub
+    
+End Module
+
+Public Sub register ()
+
+
+    Daten von Leo 
+    Verbindung zu Datenbank herstellen
+    Neuen User anlegen und abspeichern
+
 
     End Sub
 
-End Module
+
+Public Sub logincheck ()
+    Daten von Leo
+    Verbindung mit databaseuser
+    Daten überprüfen und bestätigen/Fehlermeldung
+    
+
+
+
+End Sub 
