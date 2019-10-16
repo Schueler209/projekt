46dd44cbdf8a9304bafd665ac6adec4d04a73374
Schueler209 <schueler109@jbs-pinneberg.de>
2019-10-16 15:17:19 +0200

    Register in Datenbank Test 1

--- Server/Module1.vb
+++ Server/Module1.vb
@@ -1,17 +1,21 @@
 ﻿Module Module1
 
     Public Sub Main()
-
     End Sub
-    Public Sub register()
+    Public Sub register(name As String, password As String,username As String)
 
+      Dim con As New OleDb.OleDbConnection 
+      Dim cmd As New OleDb.OleDbCommand 
+      Dim reader As OleDb.OleDbDataReader 
+      con.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" & "C:\Users\Marten\Source\Repos\projekt\Server\databaseuser.mdb"
+      cmd.Connection = con
+      cmd.CommandType = CommandType.Text
+      reader = cmd.ExecuteReader
 
-        'Daten von Leo 
-        'Verbindung zu Datenbank herstellen
-        'Neuen User anlegen und abspeichern
-
-
-    End Sub
+                con.Open()
+                cmd = New SqlCommand("INSERT INTO databaseuser values('" & username & "','" & name & "','" & password & "')", con)
+                  con.Close()
+    End Sub 
 
 
     Public Sub logincheck()
