c9d96f6b0aedd88039e6074ae6b7f74988712816
0502la <lauratoni19@gmail.com>
2019-10-16 16:44:59 +0200

    login fertig

--- Forms/Login.vb
+++ Forms/Login.vb
@@ -1,4 +1,6 @@
-﻿Public Class Login
+﻿Imports Connector
+
+Public Class Login
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         If txbBenutzername.Text = "" Then lblFehler.Text = "kein Benutzername angegeben"
         If txbPasswort.Text = "" Then lblFehler.Text = "kein Passwort angegeben"
@@ -6,6 +8,9 @@
             chatten.Show()
             Me.Hide()
 
+            If lblFehler.Text = "" Then
+                connect.Login(txbBenutzername.Text, txbPasswort.Text, AddressOf loginconfirm)
+            End If
         End If
 
     End Sub
@@ -18,4 +23,14 @@
     Private Sub txbPasswort_TextChanged(sender As Object, e As EventArgs) Handles txbPasswort.TextChanged
         txbPasswort.PasswordChar = "*"
     End Sub
+    Private connect As NetClient = New NetClient
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connect.connect()
+    End Sub
+
+    Sub Loginconfirm(wert As Boolean)
+        If wert = False Then lblFehler.Text = "Passwort und Benutzername stimmen nicht überein"
+        If wert = True Then chatten.Show()
+    End Sub
 End Class
+
