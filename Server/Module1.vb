62fb4c206810c31cae15d8f04e9f6d0d81abefc2
Schueler209 <schueler109@jbs-pinneberg.de>
2019-10-16 16:13:29 +0200

    Netserver

--- Server/Module1.vb
+++ Server/Module1.vb
@@ -1,24 +1,26 @@
 ﻿Imports System.Net.Sockets
-Imports Connector
+
+
 
 Module Module1
+    Private connect As NetServer
+
+
     Public Sub Main()
+
     End Sub
-    Public Sub register(name As String, password As String,username As String)
 
-      Dim con As New OleDb.OleDbConnection 
-      Dim cmd As New OleDb.OleDbCommand 
-      Dim reader As OleDb.OleDbDataReader 
-      con.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" & "C:\Users\Marten\Source\Repos\projekt\Server\databaseuser.mdb"
-      cmd.Connection = con
-      cmd.CommandType = CommandType.Text
-      reader = cmd.ExecuteReader
+    Public Sub register(name As String, username As String, password As String)
 
-                con.Open()
-                cmd = New SqlCommand("INSERT INTO databaseuser values('" & username & "','" & name & "','" & password & "')", con)
-                  con.Close()
-    End Sub 
-
+        Dim con As New OleDb.OleDbConnection
+        Dim cmd As New OleDb.OleDbCommand
+        Dim reader As OleDb.OleDbDataReader
+        con.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" & "C:\Users\Marten\Source\Repos\projekt\Server\databaseuser.mdb" 'Mit Datenbank verbindung aufbauen
+        reader = cmd.ExecuteReader
+        con.Open() 'Verbindung aufbauen
+        cmd = New OleDb.OleDbCommand("INSERT INTO databaseuser values('" & name & "','" & username & "','" & password & "')", con) 'Eingabe von den Daten in Datenbank
+        con.Close() 'Verbindung schließen
+    End Sub
 
     Public Sub logincheck()
         'Daten von Leo
