ea3c3a4d1ebfc28cc015d2717a80a23d0efa6586
Schueler209 <schueler109@jbs-pinneberg.de>
2019-10-16 13:14:02 +0200

    Methoden geändert

--- Server/Module1.vb
+++ Server/Module1.vb
@@ -1,28 +1,26 @@
 ﻿Module Module1
-    
+
     Public Sub Main()
 
     End Sub
-    
-End Module
-
-Public Sub register ()
+    Public Sub register()
 
 
-    Daten von Leo 
-    Verbindung zu Datenbank herstellen
-    Neuen User anlegen und abspeichern
+        'Daten von Leo 
+        'Verbindung zu Datenbank herstellen
+        'Neuen User anlegen und abspeichern
 
 
     End Sub
 
 
-Public Sub logincheck ()
-    Daten von Leo
-    Verbindung mit databaseuser
-    Daten überprüfen und bestätigen/Fehlermeldung
-    
+    Public Sub logincheck()
+        'Daten von Leo
+        'Verbindung mit databaseuser
+        'Daten überprüfen und bestätigen/Fehlermeldung
 
 
 
-End Sub 
+
+    End Sub
+End Module
