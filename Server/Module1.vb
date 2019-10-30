55d6b5b81d08eee980976fe9ca7605bdfb859754
Schueler209 <schueler109@jbs-pinneberg.de>
2019-10-30 17:11:51 +0100

    doppelter OleDB Command gefixt

--- Server/Module1.vb
+++ Server/Module1.vb
@@ -5,36 +5,39 @@ Imports Server.dbTableAdapters
 
 Module Module1
 
+    Private ReadOnly ConnectionStr = "provider= microsoft.jet.oledb.4.0;data source=db.mdb;"
+
     Public Sub Main()
 
         Dim connect As NetServer = New NetServer
         connect.OnRegister = AddressOf register
+        connect.OnLogin = AddressOf logincheck
+        'connect.OnNewFriend = AddressOf freundhinzufügen
+        'connect.OnFriends = AddressOf getFriends
         connect.connect()
         'register("Till", "Till1234", "123", Sub(g As Boolean)
         '                                    End Sub)
     End Sub
 
     Public Sub register(name As String, username As String, password As String, done As Action(Of Boolean))
-        Dim connString As String = "provider= microsoft.jet.oledb.4.0; " & "data source=db.mdb;" & ""
-        Dim conn As New OleDbConnection(connString)
-        Dim command As New OleDbCommand("INSERT INTO Users ([Name],Username,[Password]) VALUES (@displayname,@username,@password);")
-        command.Connection = conn
-        command.Parameters.Add("@displayname", OleDbType.Char).Value = name
-        command.Parameters.Add("@username", OleDbType.Char).Value = username
-        command.Parameters.Add("@password", OleDbType.Char).Value = password
-
-        command.CommandType = CommandType.Text
-        command.CommandText = "select * from users where Username = '" & username & "'"
+        Dim conn As New OleDbConnection(ConnectionStr)
         conn.Open()
-        Dim reader = command.ExecuteReader
+
+        Dim checkCommand As New OleDbCommand("select * from users where Username = '" & username & "'")
+        checkCommand.Connection = conn
+        Dim reader = checkCommand.ExecuteReader
         If reader.HasRows Then
             done(False)
-            conn.Close()
         Else
+            Dim insertCommand As New OleDbCommand("INSERT INTO Users ([Name],Username,[Password]) VALUES (@displayname,@username,@password);")
+            insertCommand.Connection = conn
+            insertCommand.Parameters.Add("@displayname", OleDbType.Char).Value = name
+            insertCommand.Parameters.Add("@username", OleDbType.Char).Value = username
+            insertCommand.Parameters.Add("@password", OleDbType.Char).Value = password
+
+            insertCommand.CommandType = CommandType.Text
             Try
-                conn.Close()
-                conn.Open()
-                command.ExecuteNonQuery()
+                insertCommand.ExecuteNonQuery()
             Catch ex As Exception
                 Console.WriteLine(ex.Message)
             Finally
@@ -45,21 +48,16 @@ Module Module1
 
     Public Sub logincheck(username As String, password As String, done As Action(Of Boolean))
 
-        Dim connString As String = "provider= microsoft.jet.oledb.4.0; " & "data source=db.mdb;" & ""
-        Dim conn As New OleDbConnection(connString)
-        Dim command As New OleDbCommand()
+        Dim conn As New OleDbConnection(ConnectionStr)
+        Dim command As New OleDbCommand("SELECT * FROM Users WHERE Username = '" & username & "'And [Password] = '" & password & "'")
         command.Connection = conn
 
-        command.CommandType = CommandType.Text
-        command.CommandText = "SELECT * FROM users WHERE Username = '" & username & "'" And "select * from users where Password = '" & password & "'"
         conn.Open()
         Dim reader = command.ExecuteReader
         If reader.HasRows Then
             done(True)
-            conn.Close()
         Else
             done(False)
-            conn.Close()
         End If
     End Sub
 
@@ -69,7 +67,7 @@ Module Module1
         Dim conn As New OleDbConnection(connString)
         Dim command As New OleDbCommand()
         command.Connection = conn
-        command.CommandText = "SELECT ID-User FROM users WHERE [username] = ? "
+        command.CommandText = "SELECT ID FROM users WHERE Username = ? "
 
         command.Parameters.AddWithValue("?", username)
         conn.Open()
@@ -96,7 +94,7 @@ Module Module1
 
         command.Connection = conn
         command.CommandType = CommandType.Text
-        command.CommandText = "select * from users where username = '" & username & "'" And "select * from users where usernamehinzugefügterfreund = '" & username & "'"
+        command.CommandText = "select * from users where username = '" & username & "'And usernamehinzugefügterfreund = '" & username & "'"
 
 
 
