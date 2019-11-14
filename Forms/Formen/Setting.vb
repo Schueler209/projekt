0b6d7c522876a658040e801262f3150e9ed5746c
Till Gruschka <till.gruschka@icloud.com>
2019-11-14 09:20:28 +0100

    Namen und Benutzernamen in Settings anzeigen

--- Forms/Formen/Setting.vb
+++ Forms/Formen/Setting.vb
@@ -1,13 +1,14 @@
 ﻿
 Public Class Einstellungen
+    'Namen und Bernutzernamen anzeigen
+    Private Sub Einstellungen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtName.Text = NetworkClass.login.name
+        lblUsername.Text = NetworkClass.login.benutzername
+    End Sub
 
     Public Sub cmdSave_Click(sender As Object, e As EventArgs) Handles cmdSave.Click
         Dim NewName As String
         NewName = txtName.Text
-
     End Sub
 
-    Private Sub Einstellungen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        NetworkClass.login
-    End Sub
 End Class
