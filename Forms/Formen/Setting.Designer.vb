0823e1be6882c9f48ae0f7674162b1a50fe8f91a
Till Gruschka <till.gruschka@icloud.com>
2019-11-28 09:59:30 +0100

    Einstellungen überarbeitet

--- Forms/Formen/Setting.Designer.vb
+++ Forms/Formen/Setting.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Einstellungen
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Einstellungen
     'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
     'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.lblUsername = New System.Windows.Forms.Label()
@@ -33,7 +33,7 @@ Partial Class Einstellungen
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(165, 67)
+        Me.Label3.Location = New System.Drawing.Point(96, 44)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(151, 24)
         Me.Label3.TabIndex = 2
@@ -43,7 +43,7 @@ Partial Class Einstellungen
         '
         Me.lblUsername.AutoSize = True
         Me.lblUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblUsername.Location = New System.Drawing.Point(369, 70)
+        Me.lblUsername.Location = New System.Drawing.Point(300, 47)
         Me.lblUsername.Name = "lblUsername"
         Me.lblUsername.Size = New System.Drawing.Size(109, 20)
         Me.lblUsername.TabIndex = 3
@@ -53,7 +53,7 @@ Partial Class Einstellungen
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(256, 139)
+        Me.Label2.Location = New System.Drawing.Point(187, 116)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(60, 20)
         Me.Label2.TabIndex = 4
@@ -62,14 +62,14 @@ Partial Class Einstellungen
         'txtName
         '
         Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtName.Location = New System.Drawing.Point(373, 136)
+        Me.txtName.Location = New System.Drawing.Point(304, 113)
         Me.txtName.Name = "txtName"
         Me.txtName.Size = New System.Drawing.Size(100, 26)
         Me.txtName.TabIndex = 5
         '
         'cmdSave
         '
-        Me.cmdSave.Location = New System.Drawing.Point(301, 199)
+        Me.cmdSave.Location = New System.Drawing.Point(232, 176)
         Me.cmdSave.Name = "cmdSave"
         Me.cmdSave.Size = New System.Drawing.Size(128, 39)
         Me.cmdSave.TabIndex = 6
