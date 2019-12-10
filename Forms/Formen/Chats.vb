9619ca562729be00d2495b46d7d862297f937357
marten.winkler <marten.winkler@LF202-06>
2019-12-10 12:27:19 +0100

    sortierung und anzeige name richtig anzeigen

--- Forms/Formen/Chats.vb
+++ Forms/Formen/Chats.vb
@@ -34,7 +34,7 @@ Public Class Chats
 
     Private Sub onGetChats(FriendChats As Chat())
         For Each chat As Chat In FriendChats
-            ltbKontakte.Items.Add(chat.user.benutzername)
+            ltbKontakte.Items.Add(chat.user.name)
             chats.Add(chat)
         Next
         If FriendChats.Length > 0 Then
@@ -65,7 +65,7 @@ Public Class Chats
         ltbKontakte.Items.Clear()
 
         For Each chat As Chat In chats
-            ltbKontakte.Items.Add(chat.user.benutzername)
+            ltbKontakte.Items.Add(chat.user.name)
         Next
         ltbKontakte.SelectedIndex = 0
     End Sub
