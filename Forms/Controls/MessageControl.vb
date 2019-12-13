a8af1872d0c00def37418aed1310a0d0cc9b7591
lauratoni19@gmail.com <lauratoni19@gmail.com>
2019-12-13 12:31:49 +0100

    backcolor

--- Forms/Controls/MessageControl.vb
+++ Forms/Controls/MessageControl.vb
@@ -20,18 +20,30 @@ Public Class MessageControl
         Label1.Text = msg.message
 
         If isOwn Then
-            Label1.BackColor = ColorTranslator.FromHtml("#73D4EC")
+
             Label2.Text = ""
         Else
-            Label1.BackColor = ColorTranslator.FromHtml("#C5D1D8")
+
             Label2.Text = msg.user.name & ":"
 
 
         End If
+        Select Case msg.user.colour
+            Case 1
+                Label1.BackColor = ColorTranslator.FromHtml("#73D4EC")
+            Case 2
+                Label1.BackColor = ColorTranslator.FromHtml("#FFD87D")
+            Case 3
+                Label1.BackColor = ColorTranslator.FromHtml("#FF7DA4")
+            Case 4
+                Label1.BackColor = ColorTranslator.FromHtml("#56B0A2")
+            Case 5
+                Label1.BackColor = ColorTranslator.FromHtml("#9BEB6F")
+        End Select
 
     End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
-    End Sub
+
+
 End Class
